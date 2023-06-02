88598a0ff763b0f6d43c6729c9c268bef20b7eac
Ivan Jankov <76175423+ivanjankov@users.noreply.github.com>
2023-06-02 11:31:49 +0200

    feat(design): make landing page responsive (#13)
    * feat: create js script for menu elements creation
    
    * feat:  add hamburger element
    
    * feat: make hero section responsive
    
    * feat: make landing page responsive

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ const displayFooterIcons = (iconsContainer, iconsArr) => {
 };
 
 displayFooterIcons(iconsWrapper, footerIcons);
+
+console.log(window.location.pathname);
+// header inlude menu
+const navMenu = document.getElementById('menu');
+const menuLinks = ['home', 'projects', 'services'];
+
+let createMenu = (array) => {
+    navMenu.innerHTML = '';
+    for (let i = 0; i < array.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.classList.add('header__menu-item');
+        let anchorItem = document.createElement('a');
+        anchorItem = setElementAttribute(
+            anchorItem,
+            'href',
+            'https://google.com'
+        );
+        anchorItem.classList.add('header__menu-item-link');
+        anchorItem.innerText = array[i];
+        listItem.appendChild(anchorItem);
+
+        navMenu.appendChild(listItem);
+    }
+};
+
+let setElementAttribute = (el, attr, attrValue) => {
+    let element = el;
+    element.setAttribute(attr, attrValue);
+
+    return element;
+};
+
+createMenu(menuLinks);
