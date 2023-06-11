cfb94761afd98298e6fdff2e3e1e0bfc5567a755
Ivan Jankov <76175423+ivanjankov@users.noreply.github.com>
2023-06-11 18:44:13 +0200

    feat(design): create hamburger menu (#15)
    * feat: include hamburger menu icons
    
    * feat: add logic to hamburger menu
    
    * feat: style hamburger menu

--- script.js
+++ script.js
@@ -15,35 +15,57 @@ const displayFooterIcons = (iconsContainer, iconsArr) => {
 
 displayFooterIcons(iconsWrapper, footerIcons);
 
-console.log(window.location.pathname);
 // header inlude menu
-const navMenu = document.getElementById('menu');
-const menuLinks = ['home', 'projects', 'services'];
+// const navMenu = document.getElementById('menu');
+// const menuLinks = ['home', 'projects', 'services'];
 
-let createMenu = (array) => {
-    navMenu.innerHTML = '';
-    for (let i = 0; i < array.length; i++) {
-        let listItem = document.createElement('li');
-        listItem.classList.add('header__menu-item');
-        let anchorItem = document.createElement('a');
-        anchorItem = setElementAttribute(
-            anchorItem,
-            'href',
-            'https://google.com'
-        );
-        anchorItem.classList.add('header__menu-item-link');
-        anchorItem.innerText = array[i];
-        listItem.appendChild(anchorItem);
+// let createMenu = (array) => {
+//     navMenu.innerHTML = '';
+//     for (let i = 0; i < array.length; i++) {
+//         let listItem = document.createElement('li');
+//         listItem.classList.add('header__menu-item');
+//         let anchorItem = document.createElement('a');
+//         anchorItem = setElementAttribute(
+//             anchorItem,
+//             'href',
+//             'https://google.com'
+//         );
+//         anchorItem.classList.add('header__menu-item-link');
+//         anchorItem.innerText = array[i];
+//         listItem.appendChild(anchorItem);
 
-        navMenu.appendChild(listItem);
-    }
+//         navMenu.appendChild(listItem);
+//     }
+// };
+
+// let setElementAttribute = (el, attr, attrValue) => {
+//     let element = el;
+//     element.setAttribute(attr, attrValue);
+
+//     return element;
+// };
+
+// createMenu(menuLinks);
+
+// Toggle hamburger menu
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const hamburgerIcon = document.querySelector('.burger-icon');
+const closeIcon = document.querySelector('.close-icon');
+const headerMenu = document.querySelector('.header__menu');
+const header = document.querySelector('.header');
+
+const toggleActiveClass = (element) => {
+    element.classList.toggle('active');
 };
 
-let setElementAttribute = (el, attr, attrValue) => {
-    let element = el;
-    element.setAttribute(attr, attrValue);
-
-    return element;
+const toggleHamburger = () => {
+    hamburgerMenu.addEventListener('click', () => {
+        toggleActiveClass(hamburgerIcon);
+        toggleActiveClass(closeIcon);
+        toggleActiveClass(headerMenu);
+        toggleActiveClass(header);
+    });
 };
 
-createMenu(menuLinks);
+toggleHamburger();
