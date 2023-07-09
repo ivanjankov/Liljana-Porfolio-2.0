4d12e22e6f181e472ac283567a5db4f9f8d93e52
Ivan Jankov <76175423+ivanjankov@users.noreply.github.com>
2023-07-09 20:41:45 +0200

    Feat(design): landing page functionality and improvements (#22)
    * fix: padding and colors
    
    * feat: style modal
    
    * feat: add functionality to modal
    
    * fix: remove comments
    
    * fix: add services to the object

--- script.js
+++ script.js
@@ -44,3 +44,95 @@ const splide = new Splide('.splide', {
     },
 });
 splide.mount();
+
+// Modal
+
+let modal = document.querySelector('.modal');
+let modalContent = document.querySelector('.modal_content');
+let body = document.querySelector('.main');
+let closeButtonModal = document.querySelector('.modal_button-close');
+let modalList = document.querySelector('.modal-list');
+let servicesWrapper = document.getElementById('services-wrapper');
+
+let services = [
+    {
+        service: 'Corporate Identity',
+        description: 'Logo and brand identity',
+        content: ['service 1', 'service 2', 'service 3'],
+    },
+    {
+        service: 'Social Media Content',
+        description: 'Social media post and marketing',
+        content: ['service 5', 'service 6', 'service 7'],
+    },
+    {
+        service: 'Online Advertising',
+        description: ' Banners, apps and pop-ups',
+        content: ['service 9', 'service 8', 'service 10'],
+    },
+    {
+        service: 'Print Advertisments',
+        description: ' Printed advertisments',
+        content: ['service 11', 'service 12', 'service 13'],
+    },
+];
+
+closeButtonModal.addEventListener('click', (e) => {
+    hideModal();
+});
+
+modalContent.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+modal.addEventListener('click', hideModal);
+
+function createServices(servicesArray) {
+    servicesArray.forEach((el) => {
+        let service = document.createElement('div');
+        service.classList.add('section__service');
+        let serviceTitle = document.createElement('h3');
+        serviceTitle.classList.add('section__service-title');
+        serviceTitle.innerText = el.service;
+        let serviceDescription = document.createElement('p');
+        serviceDescription.classList.add('section__service-description');
+        serviceDescription.innerText = el.description;
+
+        let serviceButton = document.createElement('button');
+        serviceButton.classList.add('service-link');
+        serviceButton.innerText = 'See what is included';
+
+        if (window.innerWidth < 992) {
+            serviceButton.style.maxHeight = '40px';
+        }
+        serviceButton.addEventListener('click', () => {
+            showModal();
+            populariseModal(el.content);
+        });
+        service.append(serviceTitle, serviceDescription, serviceButton);
+        servicesWrapper.appendChild(service);
+    });
+}
+
+createServices(services);
+
+function showModal() {
+    console.log('modal showed');
+    body.classList.add('main-modal-active');
+    modal.classList.add('active');
+}
+
+function hideModal() {
+    console.log('modal hidden');
+
+    body.classList.remove('main-modal-active');
+    modal.classList.remove('active');
+}
+
+function populariseModal(serviceContent) {
+    modalList.innerHTML = '';
+    serviceContent.forEach((detail) => {
+        let listItem = document.createElement('li');
+        listItem.innerText = detail;
+        modalList.appendChild(listItem);
+    });
+}
