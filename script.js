61745ff3ff72c289019e5b535ca1e7f01da4f823
Ivan Jankov <ivanjankov07@gmail.com>
2023-08-08 19:04:01 +0200

    fix: modal content

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ footerSplide.mount();
 // Modal
 
 let modal = document.querySelector('.modal');
-let modalContent = document.querySelector('.modal_content');
+let modalContent = document.querySelector('.modal_content-services');
 let body = document.querySelector('.main');
 let closeButtonModal = document.querySelector('.modal_button-close');
 let modalList = document.querySelector('.modal-list');
@@ -70,14 +70,80 @@ let services = [
     {
         service: 'Brand Identity',
         description:
-            'Logo, color palette, fonts, typography, shapes and patterns, consistent styles, brand guidlines',
-        content: ['service 1', 'service 2', 'service 3'],
+            'Social media posts, story templates, post templates, highlight covers',
+        content: {
+            serviceOne: {
+                title: 'Regular Package',
+                description:
+                    'A regular package designed for small businesses requiring essential branding elements to either establish a new brand or update an existing one',
+                includedService: [
+                    'Number of Revisions: 5',
+                    'Number of Initial concepts: 2',
+                    'Colour palette',
+                    'Typography pairing',
+                    'Mockups',
+                ],
+                finalFiles: [
+                    'Brand Guideline Sheet',
+                    'Primary logo',
+                    'Secondary logo',
+                    'Logo mark',
+                    'Logo Transparency',
+                    'High Resolution',
+                    'Vector File',
+                ],
+                footer: 'Delivery Time 7 Days',
+            },
+            serviceTwo: {
+                title: 'Social Package',
+                description:
+                    'A social package tailored for small and medium-sized online businesses that sell products or services on social networks. This package is designed to help them create a cohesive and consistent online presence across all their social media platforms.',
+                includedService: [
+                    'Number of Revisions: 5',
+                    'Number of Initial concepts: 3',
+                    'Colour palette',
+                    'Typography pairing',
+                    'Mockups',
+                ],
+                finalFiles: [
+                    'Standard Package + Story templates x3',
+                    'Post templates x12',
+                    'Highlight covers x5',
+                ],
+                footer: 'Delivery Time 7-14 days',
+            },
+        },
     },
     {
-        service: 'Social Media Content',
+        service: 'Social Media',
         description:
             'Social media posts, story templates, post templates, highlight covers',
-        content: ['service 5', 'service 6', 'service 7'],
+        content: {
+            serviceOne: {
+                title: 'Regular Package',
+                description:
+                    'Social media templates are ready-to-use formats and designs that simplify the process of crafting various types of content for the platform. These templates encompass stories, quotes, image posts, grids, and collage layouts, enabling effortless content creation.',
+                includedService: [
+                    'Story templates x3',
+                    'Post templates x12',
+                    'Highlight covers x5',
+                ],
+                finalFiles: [],
+                footer: 'Delivery Time 7 Days',
+            },
+            serviceTwo: {
+                title: 'Package Plus',
+                description:
+                    'A package plus tailored for small and medium-sized online businesses that sell products or services on social networks. This package is designed to help them create a cohesive and consistent online presence across all their social media platforms.',
+                includedService: [
+                    'Story templates x6',
+                    'Post templates x24',
+                    'Highlight covers x9',
+                ],
+                finalFiles: [],
+                footer: 'Delivery Time 7-14 days',
+            },
+        },
     },
     {
         service: 'Online advertising ',
@@ -145,10 +211,49 @@ function hideModal() {
 }
 
 function populariseModal(serviceContent) {
-    modalList.innerHTML = '';
-    serviceContent.forEach((detail) => {
-        let listItem = document.createElement('li');
-        listItem.innerText = detail;
-        modalList.appendChild(listItem);
-    });
+    modalContent.innerHTML = '';
+    const serviceOne = serviceContent.serviceOne;
+    const serviceTwo = serviceContent.serviceTwo;
+
+    if (serviceOne) {
+        modalContent.innerHTML = `
+        <div class="modal_first-service">
+            <h3 class="modal-service-title">${serviceOne.title}</h3>
+            <div class="modal-service-description">${
+                serviceOne.description
+            }</div>
+            <ul class="modal-service-included">${serviceOne.includedService
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <ul class="modal-service-included">${serviceOne.finalFiles
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <footer class="modal-service-footer">${serviceOne.footer}</footer>
+        </div>
+    
+        <div class="modal_second-service">
+            <h3 class="modal-service-title">${serviceTwo.title}</h3>
+            <div class="modal-service-description">${
+                serviceTwo.description
+            }</div>
+            <ul class="modal-service-included">${serviceTwo.includedService
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <ul class="modal-service-included">${serviceTwo.finalFiles
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <footer class="modal-service-footer">${serviceTwo.footer}</footer>
+        </div>`;
+    } else {
+        modalContent.innerHTML =
+            '<p class="other-services">At this time, I do not provide fixed packages for this service. To get a customized price estimate, kindly fill out the enquiry form with a thorough description of your requirements. Upon receipt, I will promptly respond with a tailored quote that meets your needs.</p>';
+    }
 }
