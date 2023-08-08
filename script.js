77fb7c9c9ac95eba7e907bee94a1ac1e450aaf9c
Ivan Jankov <76175423+ivanjankov@users.noreply.github.com>
2023-08-08 19:06:47 +0200

    fix: style improvements (#26)
    * fix: landing page improvements
    
    * fix: replace assets
    
    * feat: add testimonial splide
    
    * fix: modal content

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ hamburgerMenu.addEventListener('click', () => {
 
 // Triger Splide Slider
 
-const splide = new Splide('.splide', {
+const splide = new Splide('#projects-splide', {
     type: 'loop',
     perPage: 2,
-    perMove: 1,
+    perMove: 2,
     gap: '12px',
     width: '100%',
     breakpoints: {
@@ -36,11 +36,31 @@ const splide = new Splide('.splide', {
     },
 });
 splide.mount();
+const footerSplide = new Splide('#footer-splide', {
+    type: 'loop',
+    perPage: 1,
+    perMove: 1,
+    gap: '12px',
+    width: '100%',
+    arrows: false,
+    autoplay: true,
+    interval: 5000,
+    pagination: false,
+    breakpoints: {
+        768: {
+            perPage: 2,
+        },
+        576: {
+            perPage: 1,
+        },
+    },
+});
+footerSplide.mount();
 
 // Modal
 
 let modal = document.querySelector('.modal');
-let modalContent = document.querySelector('.modal_content');
+let modalContent = document.querySelector('.modal_content-services');
 let body = document.querySelector('.main');
 let closeButtonModal = document.querySelector('.modal_button-close');
 let modalList = document.querySelector('.modal-list');
@@ -48,23 +68,93 @@ let servicesWrapper = document.getElementById('services-wrapper');
 
 let services = [
     {
-        service: 'Corporate Identity',
-        description: 'Logo and brand identity',
-        content: ['service 1', 'service 2', 'service 3'],
+        service: 'Brand Identity',
+        description:
+            'Social media posts, story templates, post templates, highlight covers',
+        content: {
+            serviceOne: {
+                title: 'Regular Package',
+                description:
+                    'A regular package designed for small businesses requiring essential branding elements to either establish a new brand or update an existing one',
+                includedService: [
+                    'Number of Revisions: 5',
+                    'Number of Initial concepts: 2',
+                    'Colour palette',
+                    'Typography pairing',
+                    'Mockups',
+                ],
+                finalFiles: [
+                    'Brand Guideline Sheet',
+                    'Primary logo',
+                    'Secondary logo',
+                    'Logo mark',
+                    'Logo Transparency',
+                    'High Resolution',
+                    'Vector File',
+                ],
+                footer: 'Delivery Time 7 Days',
+            },
+            serviceTwo: {
+                title: 'Social Package',
+                description:
+                    'A social package tailored for small and medium-sized online businesses that sell products or services on social networks. This package is designed to help them create a cohesive and consistent online presence across all their social media platforms.',
+                includedService: [
+                    'Number of Revisions: 5',
+                    'Number of Initial concepts: 3',
+                    'Colour palette',
+                    'Typography pairing',
+                    'Mockups',
+                ],
+                finalFiles: [
+                    'Standard Package + Story templates x3',
+                    'Post templates x12',
+                    'Highlight covers x5',
+                ],
+                footer: 'Delivery Time 7-14 days',
+            },
+        },
     },
     {
-        service: 'Social Media Content',
-        description: 'Social media post and marketing',
-        content: ['service 5', 'service 6', 'service 7'],
+        service: 'Social Media',
+        description:
+            'Social media posts, story templates, post templates, highlight covers',
+        content: {
+            serviceOne: {
+                title: 'Regular Package',
+                description:
+                    'Social media templates are ready-to-use formats and designs that simplify the process of crafting various types of content for the platform. These templates encompass stories, quotes, image posts, grids, and collage layouts, enabling effortless content creation.',
+                includedService: [
+                    'Story templates x3',
+                    'Post templates x12',
+                    'Highlight covers x5',
+                ],
+                finalFiles: [],
+                footer: 'Delivery Time 7 Days',
+            },
+            serviceTwo: {
+                title: 'Package Plus',
+                description:
+                    'A package plus tailored for small and medium-sized online businesses that sell products or services on social networks. This package is designed to help them create a cohesive and consistent online presence across all their social media platforms.',
+                includedService: [
+                    'Story templates x6',
+                    'Post templates x24',
+                    'Highlight covers x9',
+                ],
+                finalFiles: [],
+                footer: 'Delivery Time 7-14 days',
+            },
+        },
     },
     {
-        service: 'Online Advertising',
-        description: ' Banners, apps and pop-ups',
+        service: 'Online advertising ',
+        description:
+            'Banner ads, thumbnails, pop-ups, app store screenshots,icons, illustrations',
         content: ['service 9', 'service 8', 'service 10'],
     },
     {
-        service: 'Print Advertisments',
-        description: ' Printed advertisments',
+        service: 'Print Advertisements',
+        description:
+            'Packaging, product labels, book covers, business cards, menu, big print items such as posters and billboards',
         content: ['service 11', 'service 12', 'service 13'],
     },
 ];
@@ -121,10 +211,49 @@ function hideModal() {
 }
 
 function populariseModal(serviceContent) {
-    modalList.innerHTML = '';
-    serviceContent.forEach((detail) => {
-        let listItem = document.createElement('li');
-        listItem.innerText = detail;
-        modalList.appendChild(listItem);
-    });
+    modalContent.innerHTML = '';
+    const serviceOne = serviceContent.serviceOne;
+    const serviceTwo = serviceContent.serviceTwo;
+
+    if (serviceOne) {
+        modalContent.innerHTML = `
+        <div class="modal_first-service">
+            <h3 class="modal-service-title">${serviceOne.title}</h3>
+            <div class="modal-service-description">${
+                serviceOne.description
+            }</div>
+            <ul class="modal-service-included">${serviceOne.includedService
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <ul class="modal-service-included">${serviceOne.finalFiles
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <footer class="modal-service-footer">${serviceOne.footer}</footer>
+        </div>
+    
+        <div class="modal_second-service">
+            <h3 class="modal-service-title">${serviceTwo.title}</h3>
+            <div class="modal-service-description">${
+                serviceTwo.description
+            }</div>
+            <ul class="modal-service-included">${serviceTwo.includedService
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <ul class="modal-service-included">${serviceTwo.finalFiles
+                .map((el) => {
+                    return `<li>${el}</li>`;
+                })
+                .join('')}</ul>
+            <footer class="modal-service-footer">${serviceTwo.footer}</footer>
+        </div>`;
+    } else {
+        modalContent.innerHTML =
+            '<p class="other-services">At this time, I do not provide fixed packages for this service. To get a customized price estimate, kindly fill out the enquiry form with a thorough description of your requirements. Upon receipt, I will promptly respond with a tailored quote that meets your needs.</p>';
+    }
 }
